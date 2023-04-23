d4dd9345c4ec8922b4fa06310f044e32037efec0
BasantRay <basantray344@gmail.com>
2023-04-23 09:30:10 +0545

    first commit

---
+++ location_test.dart
@@ -0,0 +1,36 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(
+    MaterialApp(
+      home: MyApp(),
+    ),
+  );
+}
+
+class MyApp extends StatefulWidget {
+
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  @override
+  Widget build(BuildContext context) {
+    String myMargin = '15';
+    double? myMarginAsADouble;
+
+    try {
+      myMarginAsADouble = double.parse(myMargin);
+    }
+    catch (e) {
+      print(e);
+    }
+    return Scaffold(
+      body: Container(
+        margin: EdgeInsets.all(myMarginAsADouble ?? 30.0),
+        color: Colors.red,
+      ),
+    );
+  }
+}
